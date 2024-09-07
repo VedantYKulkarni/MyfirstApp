ca5852481e83a2ba198d5b0d4df11e9294e203f2
VedantYKulkarni <99426561+VedantYKulkarni@users.noreply.github.com>
2024-09-08 00:13:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,6 +97,14 @@ df_prediction_proba.columns = ['Adelie','Chindtrap','Gentoo']
 df_prediction_proba.rename(columns={0:"Adelie",
                                    1:"Chinstrap",
                                    2:"Gentoo"})
+
+#Display predicted species
+st.subheader("Predicted Species")
 df_prediction_proba
+
+penguins_species = np.array(["Adelie","Chinstrap","Gentoo"])
+st.success(str(penguins_species[prediction][0]))
+
+
 
 
