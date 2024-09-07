69ebfc676e7cc33916d04a32612e4ac2873296e1
VedantYKulkarni <99426561+VedantYKulkarni@users.noreply.github.com>
2024-09-07 14:47:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ with st.sidebar:
   data = { "island":island,
           "gender":gender,
           "bill_length_mm":bill_length_mm,
-          "bill_depth_mm":ill_depth_mm,
+          "bill_depth_mm":bill_depth_mm,
           "flipper_length_mm":flipper_length_mm,
           "body_mass_g":body_mass_g
          }
