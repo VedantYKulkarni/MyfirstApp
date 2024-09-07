4f8c11c0ea0f88f1aa9fb323a00ae115cd760075
VedantYKulkarni <99426561+VedantYKulkarni@users.noreply.github.com>
2024-09-07 14:08:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.title(' Machine Learning App')
 
-st.write('This app builds a machine learning model')
+st.info('This app builds a machine learning model')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df
