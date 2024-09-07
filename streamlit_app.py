4b4cc9f39ddb0fbea1e5479b1510a369e6a78286
VedantYKulkarni <99426561+VedantYKulkarni@users.noreply.github.com>
2024-09-07 15:55:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,12 @@ with st.expander("Data"):
   df
   
   st.write("**X**")
-  X = df.drop('species', axis = 1)
-  X 
+  X_raw = df.drop('species', axis = 1)
+  X_raw 
 
   st.write("**y**")
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 
 with st.expander("Data visualization"):
   st.scatter_chart(data = df, x ='bill_length_mm', y = 'body_mass_g',color = 'species' )
@@ -41,7 +41,7 @@ with st.sidebar:
           "sex": gender
          }
   input_df = pd.DataFrame(data,index= [0])
-  input_penguins = pd.concat([input_df,X],axis = 0)
+  input_penguins = pd.concat([input_df,X_raw],axis = 0)
 
 #Encode X
 
@@ -52,6 +52,14 @@ df_penguins[:1]
 
 # Encode Y 
 
+target_mapper = {
+  "Adelie":0,
+  "Chinstrap" : 1,
+  "Gentoo":2
+}
+def target_encode(val)
+  return target_mapper[val]
+  
 with st.expander("Input features"):
   st.write("**Input penguins**")
   input_df
