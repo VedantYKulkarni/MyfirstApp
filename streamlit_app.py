ab8cd38a5558397807196107a3ba8143823feadf
VedantYKulkarni <99426561+VedantYKulkarni@users.noreply.github.com>
2024-09-07 16:05:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,15 +47,15 @@ with st.sidebar:
 
 encode = ["island","sex"]
 df_penguins = pd.get_dummies(input_penguins,prefix = encode)
-df_penguins[:1]
+input_row = df_penguins[:1]
 
 
 # Encode Y 
 
 target_mapper = {
-  "Adelie":0,
-  "Chinstrap" : 1,
-  "Gentoo":2
+"Adelie":0,
+"Chinstrap" : 1,
+"Gentoo":2
 }
 def target_encode(val):
   return target_mapper[val]
